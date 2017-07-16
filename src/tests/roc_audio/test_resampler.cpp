7be09791524c816e446dcc895a247050043f5ddb
Mikhail Baranov <baranov.mv@gmail.com>
2017-07-16 14:31:56 +0300

    resampler: discovered bug, trying to locate it

--- src/tests/roc_audio/test_resampler.cpp
+++ src/tests/roc_audio/test_resampler.cpp
@@ -57,7 +57,7 @@ TEST_GROUP(resampler) {
             spectrum[i*2] = (double)buf->data()[i];
             spectrum[i*2 + 1] = 0; // imagenary part.
         }
-        FreqSpectrum(spectrum, sig_len);
+        // FreqSpectrum(spectrum, sig_len);
     }
 
     // Generates additive white Gaussian Noise samples with zero mean and a standard deviation of 1.
@@ -141,26 +141,50 @@ TEST(resampler, upscaling_twice_single) {
 // Check upsampling quality and the cut-off band with white noise.
 TEST(resampler, upscaling_twice_awgn) {
     CHECK(resampler->set_scaling(0.5f));
-    const size_t sig_len = 2048;
+    const size_t sig_len = 1024*10;
     double buff[sig_len*2];
     size_t i;
+
+    FILE *fout = fopen("/tmp/resampler.out", "w+");
+    CHECK(fout);
 
     for (size_t n = 0; n < InSamples; n++) {
         const packet::sample_t s = (packet::sample_t)AWGN_generator();
         reader.add(1, s);
+        if (n*2 < ROC_ARRAY_SIZE(buff)) {
+            buff[n*2] = s;
+            buff[n*2+1] = 0;
+            if (n == 0){
+                fprintf(fout, "%.16f", s);
+            } else {
+                fprintf(fout, ", %.16f", s);
+            }
+        }
     }
 
     // Put the spectrum of the resampled signal into buff.
     // Odd elements are magnitudes in dB, even elements are phases in radians.
     get_sample_spectrum(buff, sig_len);
 
-    for (i = 0; i < sig_len-1; i += 2) {
-        if (i <= sig_len * 0.90 / 2){
-            CHECK(buff[i] >= -60);
-        } else if (i >= sig_len * 1.00 / 2){
-            CHECK(buff[i] <= -60);
-        }
+    fprintf(fout, "\n");
+    for (i = 0; i < ROC_ARRAY_SIZE(buff)/2-1; i += 1) {
+        fprintf(fout, "%.16f, ", buff[i]);
     }
+    fprintf(fout, "%.16f\n", buff[i]);
+    // for (i = 1; i < ROC_ARRAY_SIZE(buff)-1; i += 2) {
+    //     fprintf(fout, "%f, ", buff[i]);
+    // }
+    // fprintf(fout, "%f\n", buff[i]);
+
+    fclose(fout);
+
+    // for (i = 0; i < sig_len-1; i += 2) {
+    //     if (i <= sig_len * 0.90 / 2){
+    //         CHECK(buff[i] >= -60);
+    //     } else if (i >= sig_len * 1.00 / 2){
+    //         CHECK(buff[i] <= -60);
+    //     }
+    // }
 }
 
 TEST(resampler, downsample) {
